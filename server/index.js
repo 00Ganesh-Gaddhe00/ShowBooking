1abc7d1352330d1da87a51e8c5a5742c8832efea
Ganesh <00mr.lg00@gmail.com>
2024-08-31 14:22:16 +0530

    deployment Adustment

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 const express = require("express")
 require("./dbConfig.js");
 require('dotenv').config()
-// const cors = require("cors");
-
+var cors = require('cors')
 
 
 
@@ -19,7 +18,7 @@ const port = process.env.PORT || 5000
 
 
 // app.use(cors())
-
+app.use(cors())
 app.use(express.json())
 app.use(express.static('../client/build'))
 app.use("/api/users", UserRoutes)
